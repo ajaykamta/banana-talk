50015849af8a8c6cbbbaccffb9eaf1634221ba6c
Ajay Krishan Kamta <kamta03@live.com>
2022-01-25 01:51:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// button click
+var translateButton = document.querySelector("#translate-button");
+
+// read input
+var translateInput = document.querySelector("#translate-input");
+
+// show output
+var translateOutput = document.querySelector("#translate-output");
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+translateButton.addEventListener("click", function buttonClickHandler() {
+    var input = translateInput.value;
+    var finalURL = constructURL(input);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(jason => {
+            translateOutput.innerHTML = json.contents.translated;
+        })
+        .catch(errorHandler)
+
+})
+
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`;
+}
+
+function errorHandler(error) {
+    console.log("unexpected error :", error);
+}
